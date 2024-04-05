2d67b78b623feb2640a368c5645c30b7f8365db1
Tusharpanchariya <tusharpancharia1357@gmail.com>
2024-04-05 12:09:34 +0530

    multipilaxtion

--- re.js
+++ re.js
@@ -1,20 +1,50 @@
 // //write a code to print 1to 5 with setTieoutout
 // // expexcted 1 2 3 4 5   6 6 6 6 6
 
+// function x(){
+// for(var i=1; i<=6;i++){
+//     function end(){
+
+//         setTimeout(function (){
+//             console.log(i);
+
+//         });
+//     }
+
+
+// }
+
+
+// }
+function main(){
 function x(){
-for(var i=1; i<=5;i++){
-    function end(i){
-
-        setTimeout(function (){
-            console.log(i);
-
-        });
+    var a =4;
+    function y(){
+        console.log(a);
     }
-    end(i);
+y();
+}
+x();
+}
+main();
 
 
+
+function x(){
+for( var i=1;i<=10;i++){
+    if(  i % 2=== 0){
+    function y(i){
+        
+    setTimeout(function(){
+        console.log(i);
+
+
+    }, i*2000);
 }
 
+y(i);
+}
 
+}
 }
 x();
---
+++ table.js
@@ -0,0 +1,20 @@
+function table(n){
+    for(i=1; i<=10;i++){
+
+        function numbertable (){
+            console.log(n*i);
+
+
+        }
+        numbertable();
+
+
+
+    }
+    
+
+}
+
+table(2);
+table(3);
+table(5);
