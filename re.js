c9951267388cd7e28e89d6e898e239b3fb4ef1c1
Tusharpanchariya <tusharpancharia1357@gmail.com>
2024-04-05 12:26:53 +0530

    number-check

--- re.js
+++ re.js
@@ -1,50 +1,20 @@
-// //write a code to print 1to 5 with setTieoutout
-// // expexcted 1 2 3 4 5   6 6 6 6 6
+function x()      // number check odd or even
+{
+    for( let i=1;i<=10;i++)
+    {
+        setTimeout(function(){
+        if(i%2=== 0)
+        {
+            console.log(i+" "+ "is Even");
+        }
+        else{
+            console.log(i+" "+ "is odd");
+        }
+      
 
-// function x(){
-// for(var i=1; i<=6;i++){
-//     function end(){
-
-//         setTimeout(function (){
-//             console.log(i);
-
-//         });
-//     }
-
-
-// }
-
-
-// }
-function main(){
-function x(){
-    var a =4;
-    function y(){
-        console.log(a);
+          
+        },2000);
     }
-y();
-}
-x();
-}
-main();
-
-
-
-function x(){
-for( var i=1;i<=10;i++){
-    if(  i % 2=== 0){
-    function y(i){
-        
-    setTimeout(function(){
-        console.log(i);
-
-
-    }, i*2000);
 }
 
-y(i);
-}
-
-}
-}
 x();
